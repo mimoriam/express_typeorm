2899bf6b61e8dfcd082a89a64e3c0d11b408b827
Mimo <mimoriam420@gmail.com>
2022-12-05 18:37:25 +0500

    Commit #19 - Selection done

--- src/controllers/product.ts
+++ src/controllers/product.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import { AppDataSource } from "../index";
 import { Product } from "../entity/product.pagination";
+import { FindOptionsSelect } from "typeorm";
 
 export const GetProducts = async (
   req: Request,
@@ -8,8 +9,38 @@ export const GetProducts = async (
   next: NextFunction
 ) => {
   const productsRepository = await AppDataSource.getRepository(Product);
+  let selected, count;
 
-  const take = 1; // This is the amount of results per page shown
+  if (JSON.stringify(req.query) !== "{}") {
+    const reqQuery = { ...req.query };
+    console.log(reqQuery);
+
+    let reqQuerySelected = reqQuery["select"] as string;
+    reqQuerySelected = reqQuerySelected.replace(/,/g, " ");
+
+    let arr = reqQuerySelected.split(" ");
+    console.log(arr);
+
+    if (arr.length === 1) {
+      [selected, count] = await productsRepository.findAndCount({
+        select: [arr[0] as keyof Product],
+      });
+    } else if (arr.length === 2) {
+      [selected, count] = await productsRepository.findAndCount({
+        select: [arr[0] as keyof Product, arr[1] as keyof Product],
+      });
+    } else if (arr.length === 3) {
+      [selected, count] = await productsRepository.findAndCount({
+        select: [
+          arr[0] as keyof Product,
+          arr[1] as keyof Product,
+          arr[2] as keyof Product,
+        ],
+      });
+    }
+  }
+
+  const take = 2; // This is the amount of results per page shown
   const page = parseInt((req.query.page as string) || "1");
 
   const startIndex = (page - 1) * take;
@@ -37,6 +68,13 @@ export const GetProducts = async (
       page: page - 1,
       limit: take,
     };
+  }
+
+  if (selected) {
+    return res.send({
+      count: count,
+      selected,
+    });
   }
 
   res.send({
